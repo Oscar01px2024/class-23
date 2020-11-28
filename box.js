3dba0a898436d9890fb932e5085313ad5246484d
Oscar01px2024 <74083164+Oscar01px2024@users.noreply.github.com>
2020-11-28 10:59:15 +0800

    Add files via upload

---
+++ box.js
@@ -0,0 +1,23 @@
+class Box {
+  constructor(x, y, width, height) {
+    var options = {
+        'restitution':0.8
+    }
+    this.body = Bodies.rectangle(x, y, width, height, options);
+    this.width = width;
+    this.height = height;
+    
+    World.add(world, this.body);
+  }
+  display(){
+    var pos =this.body.position;
+    var a = this.body.angle; 
+    push();
+    translate(pos.x, pos.y);
+    rotate(a);
+    rectMode(CENTER);
+    fill(255);
+    rect(0, 0, this.width, this.height);
+    pop();
+  }
+};
